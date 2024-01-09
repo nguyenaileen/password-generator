7cba49576fde54a7ca021fb492e5efc77ab6351d
Aileen Nguyen <aileenn@gmail.com>
2024-01-08 16:55:56 -0800

    added pseduo code

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () {
+  // ask for number of characters
+
+  // ask if user wants lowercase in password
+
+  //ask if user wants uppercase in password
+
+  // ask if user wants special characters in password
+
+  //for each character in the password 
+    //select a character chosen character type randomly
+    
+    // randomly select a character from chosent character type
+
+    //add it to the password
+
+
+}
