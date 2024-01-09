da978352e28de7aa831b154db5b2e86e1d79d12f
Aileen Nguyen <aileenn@gmail.com>
2024-01-08 22:58:36 -0800

    added if statement for error message if no types of character are chosen

--- Assets/script.js
+++ Assets/script.js
@@ -26,31 +26,39 @@ function generatePassword () {
   var pwLength = prompt ("How many characters would you like your password to be? \nPlease enter a numerical value from 8-128.\n ")
 
   //return error if not a numerical from 8-128
+  //return error message if not numerical value
   if (isNaN(pwLength)){
     alert("Error! Please enter a numerical value. \nPlease choose a numerical value between 8-128.\n");
     return generatePassword;
+  //return error message if less than 8 
   }else if (pwLength < 8){
     alert("Error! Your choice was less than 8. \nPlease choose a password length between 8-128 characters.\n");
     return generatePassword;
+  //return error message if less than 8 
   }else if (pwLength > 128){
     alert("Error! Your choice was more than 128. \nPlease choose a password length between 8-128 characters.\n");
     return generatePassword;
   
   }else{
 }
+ 
+  // ask if user wants lowercase characters in password
+  var charLower = confirm ("Would you like lowercase letters in your password? \nPress Ok for yes and Cancel for no.\n")
 
-    
-  // ask if user wants lowercase in password
-  var charLower = confirm ("Would you like lowercase letters in your password? \nPress ok for yes and cancel for no.\n")
-
-  //ask if user wants uppercase in password
-  var charUpper = confirm ("Would you like uppercase letters in your password? \nPress ok for yes and cancel for no.\n")
+  //ask if user wants uppercase characters in password
+  var charUpper = confirm ("Would you like uppercase letters in your password? \nPress Ok for yes and Cancel for no\n")
 
   // ask if user wants numbers in password
-  var charNumber = confirm ("Would you like numbers in your password? \nPress ok for yes and cancel for no.\n")
+  var charNumber = confirm ("Would you like numbers in your password? \nPress Ok for yes and Cancel for no\n")
 
   // ask if user wants special characters in password
-  var charSpecial = confirm ("Would you like special characters in your password? \nPress ok for yes and cancel for no.\n")
+  var charSpecial = confirm ("Would you like special characters in your password? \nPress Ok for yes and Cancel for no\n")
+
+  //Error message if no character type is selected 
+  if (!charLower && !charUpper && !charNumber && !charSpecial){
+    alert("Error! Please choose at least one type of character to be in your password. \nPlease try again.\n");
+    return generatePassword;
+  }
 
   //for each character in the password 
     //select a character chosen character type randomly
