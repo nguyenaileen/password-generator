015f9204c684ac8a94441096561a8093722a361f
Aileen Nguyen <aileenn@gmail.com>
2024-01-08 18:45:52 -0800

    new else if statement to check if user intput is right

--- Assets/script.js
+++ Assets/script.js
@@ -25,8 +25,21 @@ function generatePassword () {
   // ask for number of characters
   var pwLength = prompt ("How many characters would you like your password to be? \nPlease enter a numerical value from 8-128.\n ")
 
-    //check if user entered a number between 8-128
+  //return error if not a numerical from 8-128
+  if (isNaN(pwLength)){
+    alert("Error! Please enter a numerical value. \nPlease choose a numerical value between 8-128.\n");
+    return generatePassword;
+  }else if (pwLength < 8){
+    alert("Error! Your choice was less than 8. \nPlease choose a password length between 8-128 characters.\n");
+    return generatePassword;
+  }else if (pwLength > 128){
+    alert("Error! Your choice was more than 128. \nPlease choose a password length between 8-128 characters.\n");
+    return generatePassword;
+  
+  }else{
+}
 
+    
   // ask if user wants lowercase in password
   var charLower = confirm ("Would you like lowercase letters in your password? \nPress ok for yes and cancel for no.\n")
 
